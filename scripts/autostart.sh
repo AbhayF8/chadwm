a5c6fd1a42f6be44aa21e9cde083e44932f19408
Abhay Patel <patelabhayindia@gmail.com>
2022-04-07 14:12:40 +0530

    adding my autostart apps

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -6,4 +6,16 @@
 feh --bg-fill ~/Pictures/wall/wall.png &
 # run bar script and dwm ( do not remove this )
 bash ~/.config/chadwm/scripts/./dwmbar.sh &
+
+#
+safeeyes &
+light -S 50.2
+# stalonetray --icon-size=16 --kludges=force_icons_size &
+redshift &
+if 
+[[ $(xrandr -q | grep " connected" | cut -d ' ' -f1 | tail -n 1) == "HDMI-A-0" ]]
+then 
+alacritty -e kodi &
+fi
+#
 /usr/local/bin/dwm
