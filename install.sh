d575d46803428f5b8d99b5e5c88d4a6a06970eed
Abhay Patel <patelabhayindia@gmail.com>
2022-04-07 13:13:04 +0530

    Fixing install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
+echo "Going to temp directory"
+sleep 1
+cd /tmp
+sleep 1
+echo "Cloning"
+git clone "https://github.com/AbhayF8/chadwm"
+sleep 1
+echo "moving to the cloned directory"
+cd chadwm
 echo "moving chadwm directory to the .config directory"
 mv chadwm ~/.config
 sleep 1
 echo "changing directory to chadwm/chadwm inside .config that was just copied"
-cd ~/.config/chadwm/chadwm
+cd ~/.config/chadwm
 sleep 1
 echo "Copying the required fonts"
 sleep 1
@@ -12,7 +21,7 @@ echo "Making the dwm using make"
 sudo make install
 sleep 1
 echo "Creating a desktop entry with name chadwm.desktop which will run the autostart file inside chadwm"
-touch /usr/share/xsessions/chadwm.desktop  
+sudo touch /usr/share/xsessions/chadwm.desktop  
 sleep 1
 sudo cp chadwm.desktop /usr/share/xsessions/chadwm.desktop
 echo 'Copying desktop file for starting a xsession'
