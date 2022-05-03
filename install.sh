45dc433ca1ad5202790b72aa35aae1b53aaaacd0
Abhay Patel <patelabhayindia@gmail.com>
2022-05-03 15:42:07 +0530

    Improving install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,29 @@
 #!/bin/sh
-echo "Removing any existing chadwm install"
+echo "Removing any existing chadwm installation"
 sleep 1
 yes | rm -r ~/.config/chadwm
 echo "Going to temp directory"
-sleep 10
+sleep 1
 cd /tmp
 sleep 1
 echo "Cloning"
 git clone "https://github.com/AbhayF8/chadwm"
 sleep 1
-echo "moving to the cloned directory"
-# cd chadwm
-echo "moving chadwm directory to the .config directory"
+echo "Moving chadwm directory to the .config directory"
 mv chadwm ~/.config
 sleep 1
-echo "changing directory to chadwm/chadwm inside .config that was just copied"
+echo "Changing directory to chadwm/chadwm inside .config that was just copied"
 cd ~/.config/chadwm
 sleep 1
 echo "Copying the required fonts"
 sleep 1
 cp fonts/* ~/.local/share/fonts
-echo "Making the dwm using make"
+echo "Making the dwm using make SUDO PASSWORD WILL BE ASKED"
 cd chadwm
 sudo make install
 sleep 1
-echo "Creating a desktop entry with name chadwm.desktop which will run the autostart file inside chadwm"
+echo "Touching and copying the chadwm.desktop desktop entry file which will run the autostart file inside chadwm"
 sudo touch /usr/share/xsessions/chadwm.desktop  
 sleep 1
 sudo cp ../chadwm.desktop /usr/share/xsessions/chadwm.desktop
-echo 'Copying desktop file for starting a xsession'
 echo "Done"
